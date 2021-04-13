041f10eae526c81cb669595ee88ac0d1302a371d
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-04-13 10:12:15 +0600

    Test Java Main Class

---
+++ ooad_chapter_01/src/com/obydul/test/TestJava.java
@@ -0,0 +1,15 @@
+package com.obydul.test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		a = 2;
+		b =  3;
+		c = a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
